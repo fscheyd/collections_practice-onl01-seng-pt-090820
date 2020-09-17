cf7d49ea03aa69349136c3f4eed7e463a548937f
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-17 02:08:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,13 +12,7 @@ def sort_array_char_count(arr)
 end
 
 def swap_elements(array)
-  array.sort do |b, c|
-  if b == c
-    0
-  elsif b < c
-    
-  elsif a > b
-    1
+  array.sort do array[1], array[2] = array[2], array[1]
   end
 end
 
